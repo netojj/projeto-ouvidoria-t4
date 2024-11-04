7fcc61360a47d642da42f93e2d88124150578e14
José Neto <neto.s0899@outlook.com>
2024-11-04 20:34:34 -0300

    inserção do menu de opções e criação da opção 1

--- principal.py
+++ principal.py
@@ -1,3 +1,20 @@
 '''
 
 '''
+
+opcao = 1;
+manifestacoes = [ ];
+
+while opcao != 7:
+    print("\nOpções: \n1)Listagem das Manifestações\n2)Criar uma nova Manifestação\n3)Exibir quantidade de manifestações\n"
+          "4)Pesquisar uma manifestação por código\n5)Excluir uma Manifestação pelo Código\n6)Sair do Sistema.");
+    opcao = int(input("Digite sua opção: "));
+
+    if opcao == 1:
+        if len(manifestacoes) > 0:
+            print("Lista de Manifestações:")
+            for index in range(len(manifestacoes)):
+                print(index+1,"-",manifestacoes[index])
+
+        else:
+            print("Não existem manifestações a serem exibidas!")
