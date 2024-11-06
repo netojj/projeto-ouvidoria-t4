00f542f77956d14dcc7f51e490a9091238d8abad
FelipeMonteiroUnifacisa <felipe.gomes@maisunifacisa.com.br>
2024-11-06 14:34:53 -0300

    Compilado dos envios de todos os ajustes

--- principal.py
+++ principal.py
@@ -1,36 +1,94 @@
+'''
+Bruna Neves
+Ithalo Teodósio
+Esdras de Menezes
+Priscilla Lima
+André Holanda
+Felipe Monteiro
+José Neto
+Samuel Ferreira
+'''
+
 opcao = 1
 manifestacoes = []
 print("====== Ouvidoria - Universidade XYZ ======")
+menu = ('''
+    Menu de Opções:
+    1) Listagem das manifestações
+    2) Criar uma nova manifestação
+    3) Exibir quantidade de manifestações
+    4) Pesquisar uma manifestação por código
+    5) Excluir uma manifestação pelo código
+    6) Sair do sistema
+    ''')
 
-while opcao != 7:
-    print("\nOpções: \n1)Listagem das Manifestações\n2)Criar uma nova Manifestação\n3)Exibir quantidade de manifestações\n"
-          "4)Pesquisar uma manifestação por código\n5)Excluir uma Manifestação pelo Código\n6)Sair do Sistema.");
-    opcao = int(input("Digite sua opção: "))
+print(menu)
+
+while opcao != 6:
+
+    opcao = int(input("Digite a opção desejada: "))
 
     if opcao == 1:
         if len(manifestacoes) > 0:
-            print("Lista de Manifestações:")
-            for index in range(len(manifestacoes)):
-                print(index+1,"-",manifestacoes[index])
+            print("Lista de manifestações")
+            for i, item in enumerate(manifestacoes):
+                print(f"{i + 1} - {item}")
+            print("\n")
 
         else:
-            print("Não existem manifestações a serem exibidas!")
+            print("\nNão existem manifestações a serem exibidas.\n")
 
     elif opcao == 2:
-        novaManifestacao = input("Digite uma nova manifestação: ")
+        novaManifestacao = input("\nDigite aqui a sua nova manifestação: ")
         manifestacoes.append(novaManifestacao)
-        print()
-        print("Manifestação criada com sucesso!")
-        print()
+        print("Manifestação criada com sucesso!\n")
 
     elif opcao == 3:
-        palavraPlural = "manifestações cadastradas" if len(manifestacoes) > 1 else "manifestação cadastrada"
-        print(f'Temos um total de {len(manifestacoes)} {palavraPlural} em sistema');
+        quantidade = len(manifestacoes)
+        if quantidade == 0:
+            print("\nAtualmente não temos manifestações registradas em nosso sistema.\n")
+
+        elif quantidade == 1:
+            print("\nAtualmente temos 1 manifestação registrada em nosso sistema.\n")
+
+        else:
+            print("\nAtualmente temos", quantidade, "manifestações registradas em nosso sistema.\n")
 
     elif opcao == 4:
-        busca = int(input("Qual a manifestação você deseja exibir? "))
-        if busca >= 0 and busca <= len(manifestacoes):
-            print(f'Manifestação encontrada: {busca} - {manifestacoes[busca-1]}' )
-        else:
-            print('Nenhuma manifestação encontrada')
+        if len(manifestacoes) == 0:
+            print("\nNão há nenhuma manifestação para pesquisar!\n")
 
+        else:
+            codigoManifestacao = int(input("\nDigite o código da manifestação que deseja pesquisar: "))
+            if codigoManifestacao >= 1 and codigoManifestacao <= len(manifestacoes):
+                print(f"\nManifestação encontrada: {manifestacoes[codigoManifestacao - 1]} \n")
+            else:
+                print("\nManifestação não encontrada.\n")
+
+    elif opcao == 5:
+        if len(manifestacoes) == 0:
+            print("\nNão existem manifestações a serem removidas\n")
+
+        else:
+            print("\nManifestações: ")
+            for index in range(len(manifestacoes)):
+                print(index + 1, "-", manifestacoes[index])
+
+            excluirPosicao = int(input("\nDigite o código da manifestação que deseja excluir: "))
+
+            if excluirPosicao >= 1 and excluirPosicao <= len(manifestacoes):
+                confirmar = int(
+                    input(f"\nDeseja realmente remover a manifestação \"{manifestacoes[excluirPosicao - 1]}\"?  \n   1) para Sim e 2 para Não "))
+                if confirmar == 1:
+                    manifestacoes.pop(excluirPosicao - 1)
+                    print("   Manifestação removida com sucesso\n")
+                elif confirmar != 2:
+                    print("   Opção inválida\n")
+            else:
+                print("Manifestação inválida\n")
+
+    elif opcao != 6:
+        print("\nOpção Inválida! Tente novamente.")
+        print(f"{menu}")
+
+print("\n====== Obrigado por usar o programa da Ouvidoria. Até a próxima! ======")
